c08cff2a9e7a305cb0cc51978ed3b1318bff35db
ChandanaTalluri <ctalluri@gus.pittstate.edu>
2020-11-22 00:24:41 -0600

    feeding add,delete and export

--- controllers/feedingController.js
+++ controllers/feedingController.js
@@ -1,6 +1,137 @@
+const Feedings = require('../models/feeding.model');
+const Animal = require('../models/animal.model');
+const FoodType = require('../models/foodType.model');
+const Medicines = require('../models/medicines.model');
+const excel = require('exceljs');
 
-exports.get_feeding = function(req, res) {
-    
-        res.render('feeding/feeding');
-     
+exports.get_export = async function(req, res) {
+        const feedings = await Feedings.find({}).sort({dateTime: 'desc'});
+      
+        const workbook = new excel.Workbook();
+        const worksheet = workbook.addWorksheet('Feedings');
+        worksheet.columns = [
+          {header: 'Date', key: 'dateTime', width: 10},
+          {header: 'Species', key: 'animalSpecies', width: 10},
+          {header: 'Nickname', key: 'animalNickName:', width: 10},
+          {header: 'Food', key: 'food', width: 10},
+          {header: 'Medicine', key: 'medicine', width: 10},
+          {header: 'Goal Weight', key: 'goalWeightOfAnimal', width: 10},
+          {header: 'Actual Weight', key: 'actualWeightOfAnimal', width: 10},
+          {header: 'Amount Fed', key: 'amountOfFoodFed', width: 10},
+          {header: 'Leftover Food', key: 'leftoverFood', width: 10},
+          {header: 'Weather Conditions', key: 'weatherConditions', width: 20},
+          {header: 'Comments', key: 'comments', width: 50},
+        ];
+      
+        worksheet.addRows(feedings);
+      
+        res.setHeader(
+            'Content-Type',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        );
+        res.setHeader(
+            'Content-Disposition',
+            'attachment; filename=' + 'feedings.xlsx',
+        );
+        return workbook.xlsx.write(res).then(function() {
+          res.status(200).end();
+        });
+      };
+      
+
+exports.get_feedings = async function(req, res) {
+        const feedings = await Feedings.find({}).sort({dateTime: 'desc'});
+        res.render('feeding/feeding', { data: feedings });
+
 }
+
+exports.get_add_feedings = async function (req, res) {
+        const animals= await Animal.find({enabled: true});
+        const foods= await FoodType.find({});
+        const medicines= await Medicines.find({});
+        res.render('feeding/addFeedings',{animals:animals,foods:foods,medicines:medicines});
+}
+
+exports.post_add_feedings =async function (req, res) {
+
+        const animal= await Animal.findOne({_id:req.body.animalId});
+
+        let feedings = new Feedings({
+
+                animalSpecies: animal.species,
+                animalNickName: animal.nickName,
+                food: req.body.food,
+                medicine: req.body.medicine,
+                goalWeightOfAnimal: req.body.goalWeightOfAnimal,
+                actualWeightOfAnimal: req.body.actualWeightOfAnimal,
+                amountOfFoodFed: req.body.amountOfFoodFed,
+                leftoverFood: req.body.leftoverFood,
+                comments: req.body.comments,
+                weatherConditions: req.body.weatherConditions,
+                dateTime: req.body.dateTime,
+                keeperName: req.body.keeperName
+
+        });
+        console.log(feedings);
+        feedings.save(function (err) {
+                if (err) {
+                        // handle error
+                        console.log(err);
+                } else {
+                        // saved!
+                        res.redirect('/feeding')
+                }
+        });
+}
+
+
+
+exports.get_update_feedings = function (req, res) {
+
+        Feedings.findOne({ _id: req.query.id }, function (err, feedings) {
+                if (err) {
+                        // handle error
+                } else {
+                        console.log(feedings);
+                        res.render('feeding/updateFeedings', { data: feedings });
+                }
+        });
+}
+exports.post_update_feedings = function (req, res) {
+
+
+        const updateFeedings = {
+                animalSpecies: req.body.animalSpecies,
+                animalNickName: req.body.animalNickName,
+                food: req.body.food,
+                medicine: req.body.medicine,
+                goalWeightOfAnimal: req.body.goalWeightOfAnimal,
+                actualWeightOfAnimal: req.body.actualWeightOfAnimal,
+                amountOfFoodFed: req.body.amountOfFoodFed,
+                leftoverFood: req.body.leftoverFood,
+                comments: req.body.comments,
+                weatherConditions: req.body.weatherConditions,
+                dateTime: req.body.dateTime,
+                keeperName: req.body.keeperName
+        };
+
+        Feedings.findOneAndUpdate({ _id: req.body.id }, updateFeedings, function (err, data) {
+                if (err) {
+                        // handle error
+                        console.log(err);
+                } else {
+                        res.redirect('/feeding');
+                }
+        });
+};
+
+exports.get_delete_feedings = function (req, res) {
+        Feedings.findOneAndDelete({ _id: req.query.id }, function (err) {
+                if (err) {
+                        // handle error
+                        console.log(err);
+                } else {
+                        res.redirect('/feeding');
+                }
+        });
+};
--- routes/feedingRouter.js
+++ routes/feedingRouter.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 var feedingController = require('../controllers/feedingController');
-
 const authMiddleware = require('../middleware/ensureauthenticated');
 
-router.get('/',authMiddleware.ensureAuthenticated, feedingController.get_feeding);
+router.get('/',authMiddleware.ensureAuthenticated,feedingController.get_feedings);
+router.get('/export',authMiddleware.ensureAuthenticated,feedingController.get_export);
+
+router.get('/addFeedings',authMiddleware.ensureAuthenticated, feedingController.get_add_feedings);
+router.post('/addFeedings',authMiddleware.ensureAuthenticated, feedingController.post_add_feedings);
+
+router.get('/updateFeedings',authMiddleware.ensureAuthenticated, feedingController.get_update_feedings);
+router.post('/updateFeedings',authMiddleware.ensureAuthenticated, feedingController.post_update_feedings);
+
+router.get('/deleteFeedings',authMiddleware.ensureAuthenticated, feedingController.get_delete_feedings);
 
 module.exports = router;
 
