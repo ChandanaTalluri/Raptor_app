1957502e2c39587989c4b13873ba7167d778e1d5
ChandanaTalluri <ctalluri@gus.pittstate.edu>
2020-12-02 00:59:56 -0600

    manageaccount

--- controllers/manageAccountController.js
+++ controllers/manageAccountController.js
@@ -1,8 +1,10 @@
 
 const User = require('../models/user.model');
 
-exports.get_account = function(req, res) {
-    res.render('manageAccount/manageAccount');
+exports.get_account = async function(req, res) {
+    console.log(req.user._id );
+    const user = await User.findOne({ _id: req.user._id });
+    res.render('manageAccount/manageAccount',{data:user});
        
 }
 
@@ -11,20 +13,48 @@ exports.post_account = function (req, res) {
     
 }
 
-exports.get_password = function(req, res) {
+exports.get_password = async function(req, res) {
+    console.log(req.user._id );
+    const user = await User.findOne({ _id: req.user._id });
     res.render('manageAccount/changePassword');
 }
 
-exports.post_password = function(req, res) {
-    res.render('manageAccount/changePassword');
+exports.post_password = async function(req, res) {
+    console.log(req.user._id );
+    const userInfo = await User.findOne({ _id: req.user._id });
+    let oldPassword = req.body.oldPassword;
+  //  let newUser = new User();
+    
+    res.render('/manageAccount');
 }
 
-exports.get_updateAccount = function(req, res) {
-    res.render('manageAccount/updateAccountDetails');
+exports.get_updateAccount = async function(req, res) {
+    console.log(req.user._id );
+    const user = await User.findOne({ _id: req.user._id });
+    console.log(user);
+    res.render('manageAccount/updateAccountDetails',{data:user});
 }
 
-exports.post_updateAccount = function(req, res) {
-    res.render('manageAccount/updateAccountDetails');
+exports.post_updateAccount = async function(req, res) {
+    const userInfo = await User.findOne({ _id: req.user._id });
+    let updateUserAccount = new User({
+        email:req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password:userInfo.password,
+        role: userInfo.role,
+    });
+
+    console.log(updateUserAccount);
+    User.findOneAndUpdate({ _id: req.body.id }, updateUserAccount, function (err, data) {
+        if (err) {
+            // handle error
+            console.log(err);
+        } else {
+            res.redirect('/manageAccount');
+        }
+    });
+    
 }
 
 
